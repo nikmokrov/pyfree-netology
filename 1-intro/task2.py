49b31eccda4cc03099c76be6db319730ee6d6a30
Nikolay Mokrov <nikmokrov@yandex.ru>
2023-11-20 10:34:51 +0300

    Task2 done

---
+++ 1-intro/task2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+date1 = input("Введите дату: ")
+task1 = input("Введите задачу: ")
+date2 = input("Введите дату: ")
+task2 = input("Введите задачу: ")
+date3 = input("Введите дату: ")
+task3 = input("Введите задачу: ")
+print(date1, task1)
+print(date2, task2)
+print(date3, task3)
